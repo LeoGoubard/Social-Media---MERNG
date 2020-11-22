33becd7ce827bbd1b83ec0b6ea713617007e1851
LeoG <goubardleo@gmail.com>
2020-11-22 19:32:10 +0100

    phew tough one, we needed to Authorize the Header each time we send a request, for being able to post a post, apollo link were pretty usefull here

---
+++ client/src/components/PostForm.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Form, Button } from 'semantic-ui-react'
+import gql from 'graphql-tag'
+import { useMutation } from '@apollo/react-hooks'
+
+import { useForm } from '../util/hooks'
+import { FETCH_POSTS_QUERY } from '../util/graphql';
+
+
+function PostForm() {
+
+    const { values, onChange, onSubmit } = useForm(createPostCallBack, {
+        body:''
+    });
+    
+    const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
+        variables: values,
+        update(proxy, result){
+            const data = proxy.readQuery({
+                query: FETCH_POSTS_QUERY
+            })
+            const posts = [result.data.createPost, ...data.getPosts];
+            proxy.writeQuery({ query: FETCH_POSTS_QUERY, data: { getPosts: posts } });
+            values.body ='';
+        }
+    })
+
+    function createPostCallBack(){
+        createPost();
+    }
+    return (
+        <Form onSubmit={onSubmit}>
+            <h2>Create a post:</h2>
+            <Form.Field>
+                <Form.Input
+                placehodler="Hi World!"
+                name="body"
+                onChange={onChange}
+                value={values.body}
+                />
+                <Button type="submit" color="teal">
+                    Submit
+                </Button>
+            </Form.Field>
+            
+        </Form>
+    )
+}
+
+const CREATE_POST_MUTATION = gql`
+    mutation createPost($body: String!){
+        createPost(body: $body){
+            id body createdAt username
+            likes{
+                id username createdAt
+            }
+            likeCount
+            comments{
+                id body username createdAt
+            }
+            commentCount
+        }
+    }
+`
+
+export default PostForm
